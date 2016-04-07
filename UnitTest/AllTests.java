cc9bb021288fbdb39bb68785ac4350d8221e5663
chintan <chintan791@gmail.com>
2016-04-07 15:36:48 -0400

    commit message

---
+++ UnitTest/AllTests.java
@@ -0,0 +1,9 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ UnitTest.class, UnitTest2.class })
+public class AllTests {
+
+}
